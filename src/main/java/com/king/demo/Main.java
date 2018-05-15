6e7d12eab96cb52348bbd2bb25c8ab8eb0754e15
jinlong <jinlong@rongcloud.cn>
2018-05-15 18:53:49 +0800

    first commit

---
+++ src/main/java/com/king/demo/Main.java
@@ -0,0 +1,11 @@
+package com.king.demo;
+
+/**
+ * @author 金龙
+ * @date 2018/5/8 at 下午9:09
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
